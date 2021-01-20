c59a548175d7ff8c28096a4b18f93d0813b4474e
ootsukidesigning <web@de-signing.com>
2021-01-21 02:02:31 +0900

    On index redirect setting.

--- pages/index.vue
+++ pages/index.vue
@@ -4,23 +4,7 @@
       <header>
         <OrganismHeader />
       </header>
-      <section id="leadcopy" class="my-4">
-        <b-container>
-          <b-row class="mb-4">
-            <b-col>
-              <h1>ご相談・<br class="d-md-none" />お問い合わせ</h1>
-            </b-col>
-          </b-row>
-          <b-row>
-            <b-col>
-              <NuxtContent :document="index" />
-            </b-col>
-          </b-row>
-        </b-container>
-      </section>
-      <section id="form" class="my-4">
-        <!-- <OrganismFormDispatch v-bind="{ form }" /> -->
-      </section>
+      <!-- This is body -->
     </div>
 
     <footer class="bg-light">
@@ -31,10 +15,9 @@
 
 <script>
 export default {
-  async asyncData({ $content }) {
-    const index = await $content('form/index').fetch()
-    const form = await $content('form/define').fetch()
-    return { index, form }
+  async asyncData({ $content, redirect }) {
+    redirect({ name: 'form' })
+    return
   },
 }
 </script>
