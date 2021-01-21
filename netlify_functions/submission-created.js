cceed9318eb87e64f4f343926266c76bbd815098
ootsukidesigning <web@de-signing.com>
2021-01-21 15:08:37 +0900

    bug fix

--- netlify_functions/submission-created.js
+++ netlify_functions/submission-created.js
@@ -7,7 +7,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-  secure: false,
+  secure: process.env.SMTP_SECURE,
 })
 
 exports.handler = function (event, context, callback) {
